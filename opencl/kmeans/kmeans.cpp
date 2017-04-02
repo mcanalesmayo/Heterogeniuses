f5913a06254467d3d472076e7c48effe5fd06423
Jorge <646122@unizar.es>
2017-04-02 13:31:31 +0200

    New test branch

--- opencl/kmeans/kmeans.cpp
+++ opencl/kmeans/kmeans.cpp
@@ -5,6 +5,10 @@
 #include <iostream>
 #include <string>
 #include "kmeans.h"
+#include "CL/opencl.h"
+#include "AOCLUtils/aocl_utils.h"
+
+using namespace aocl_utils;
 
 #ifdef WIN
 	#include <windows.h>
@@ -49,19 +53,38 @@
      #define BLOCK_SIZE2 512
 #endif
 
+#define STRING_BUFFER_LEN 1024
+
 
 
 // local variables
-static cl_context	    context;
+//static cl_context	    context;
 static cl_command_queue cmd_queue;
 static cl_device_type   device_type;
 static cl_device_id   * device_list;
 static cl_int           num_devices;
 
+// OpenCL runtime configuration
+static cl_platform_id platform = NULL;
+static cl_device_id device = NULL;
+static cl_context context = NULL;
+static cl_command_queue queue = NULL;
+static cl_kernel kernel = NULL;
+static cl_program program;
+
+
+static void device_info_ulong( cl_device_id device, cl_device_info param, const char* name);
+static void device_info_uint( cl_device_id device, cl_device_info param, const char* name);
+static void device_info_bool( cl_device_id device, cl_device_info param, const char* name);
+static void device_info_string( cl_device_id device, cl_device_info param, const char* name);
+static void display_device_info( cl_device_id device );
+
 static int initialize(int use_gpu)
 {
 	cl_int result;
 	size_t size;
+	cl_int status;
+	cl_int err;
 
 	// create OpenCL context
 	cl_platform_id *platform_ids;
@@ -69,30 +92,92 @@ static int initialize(int use_gpu)
 	cl_uint num_platforms;
 	cl_platform_info platform_info;
 	char platform_name[30];
-	if (clGetPlatformIDs(0, NULL, &num_platforms) != CL_SUCCESS) { printf("ERROR: clGetPlatformIDs(0,NULL,&num_platforms) failed\n"); return -1; }
+
+
+	/*if (clGetPlatformIDs(0, NULL, &num_platforms) != CL_SUCCESS) { printf("ERROR: clGetPlatformIDs(0,NULL,&num_platforms) failed\n"); return -1; }
 	printf("Number of platforms: %d\n", num_platforms);
 	platform_ids = (cl_platform_id *) malloc(num_platforms * sizeof(cl_platform_id));
 	if (clGetPlatformIDs(num_platforms, platform_ids, NULL) != CL_SUCCESS) { printf("ERROR: clGetPlatformIDs(num_platforms,platform_ids,NULL) failed\n"); return -1; }
 	// NVIDIA CUDA is idx=1
-	platform_id = platform_ids[1];
+	// ALTERA is idx=0
+	platform_id = platform_ids[0];
 
 	for(int i=0; i<num_platforms; i++){
 		clGetPlatformInfo(platform_ids[i], CL_PLATFORM_NAME, 30, platform_name, NULL);
 		printf("Platform %d: %s\n", i, platform_name);
 	}
-
-	cl_context_properties ctxprop[] = { CL_CONTEXT_PLATFORM, (cl_context_properties)platform_id, 0};
+	*/
+	/*cl_context_properties ctxprop[] = { CL_CONTEXT_PLATFORM, (cl_context_properties)platform_id, 0};
 	device_type = use_gpu ? CL_DEVICE_TYPE_GPU : CL_DEVICE_TYPE_CPU;
 	context = clCreateContextFromType( ctxprop, device_type, NULL, NULL, NULL );
 	if( !context ) { printf("ERROR: clCreateContextFromType(%s) failed\n", use_gpu ? "GPU" : "CPU"); return -1; }
+	*/
 
-	// get the list of GPUs
+	// Get the OpenCL platform.
+	platform = findPlatform("Altera SDK for OpenCL");
+	if(platform == NULL) {
+	printf("ERROR: Unable to find Intel(R) FPGA OpenCL platform.\n");
+	return false;
+	}
+
+	// User-visible output - Platform information
+	{
+	char char_buffer[STRING_BUFFER_LEN]; 
+	printf("Querying platform for info:\n");
+	printf("==========================\n");
+	clGetPlatformInfo(platform, CL_PLATFORM_NAME, STRING_BUFFER_LEN, char_buffer, NULL);
+	printf("%-40s = %s\n", "CL_PLATFORM_NAME", char_buffer);
+	clGetPlatformInfo(platform, CL_PLATFORM_VENDOR, STRING_BUFFER_LEN, char_buffer, NULL);
+	printf("%-40s = %s\n", "CL_PLATFORM_VENDOR ", char_buffer);
+	clGetPlatformInfo(platform, CL_PLATFORM_VERSION, STRING_BUFFER_LEN, char_buffer, NULL);
+	printf("%-40s = %s\n\n", "CL_PLATFORM_VERSION ", char_buffer);
+	}
+
+	// Query the available OpenCL devices.
+	scoped_array<cl_device_id> devices;
+	cl_uint num_devices;
+
+	devices.reset(getDevices(platform, CL_DEVICE_TYPE_ALL, &num_devices));
+
+	// We'll just use the first device.
+	device = devices[0];
+
+	// Display some device information.
+	display_device_info(device);
+
+	// Create the context.
+	context = clCreateContext(NULL, 1, &device, &oclContextCallback, NULL, &status);
+	checkError(status, "Failed to create context");
+
+	// Create the command queue.
+	queue = clCreateCommandQueue(context, device, CL_QUEUE_PROFILING_ENABLE, &status);
+	checkError(status, "Failed to create command queue");
+
+	// Create the program.
+	std::string binary_file = getBoardBinaryFile("/home/jcancer/Heterogeniuses/opencl/kmeans/kmeans", device);
+	printf("Using AOCX: %s\n", binary_file.c_str());
+	program = createProgramFromBinary(context, binary_file.c_str(), &device, 1);
+
+	// Build the program that was just created.
+	err = clBuildProgram(program, 0, NULL, "", NULL, NULL);
+	checkError(err, "Failed to build program");
+
+	// Create the kernel - name passed in here must match kernel name in the
+	// original CL file, that was compiled into an AOCX file using the AOC tool
+	const char *kernel_name = "kmeans";  // Kernel name, as defined in the CL file
+	kernel = clCreateKernel(program, kernel_name, &err);
+	checkError(err, "Failed to create kernel");
+
+/*	// get the list of GPUs
 	result = clGetContextInfo( context, CL_CONTEXT_DEVICES, 0, NULL, &size );
-	num_devices = (int) (size / sizeof(cl_device_id));
-	
+	num_devices = (int) (size / sizeof(cl_device_id));	
 	if( result != CL_SUCCESS || num_devices < 1 ) { printf("ERROR: clGetContextInfo() failed\n"); return -1; }
+
+
 	device_list = new cl_device_id[num_devices];
 	if( !device_list ) { printf("ERROR: new cl_device_id[] failed\n"); return -1; }
+
+
 	result = clGetContextInfo( context, CL_CONTEXT_DEVICES, size, device_list, NULL );
 	if( result != CL_SUCCESS ) { printf("ERROR: clGetContextInfo() failed\n"); return -1; }
 
@@ -100,7 +185,7 @@ static int initialize(int use_gpu)
 	cmd_queue = clCreateCommandQueue( context, device_list[0], 0, NULL );
 	if( !cmd_queue ) { printf("ERROR: clCreateCommandQueue() failed\n"); return -1; }
 
-	free(platform_ids);
+	free(platform_ids);*/
 
 	return 0;
 }
@@ -127,7 +212,7 @@ cl_mem d_feature_swap;
 cl_mem d_cluster;
 cl_mem d_membership;
 
-cl_kernel kernel;
+//cl_kernel kernel;
 cl_kernel kernel_s;
 cl_kernel kernel2;
 
@@ -140,34 +225,36 @@ float *center_d;
 int allocate(int n_points, int n_features, int n_clusters, float **feature)
 {
 
-	int sourcesize = 1024*1024;
+	cl_int err;
+	
+	/*	int sourcesize = 1024*1024;
 	char * source = (char *)calloc(sourcesize, sizeof(char)); 
 	if(!source) { printf("ERROR: calloc(%d) failed\n", sourcesize); return -1; }
 
-	// read the kernel core source
+	// Read the kernel core source
 	char * tempchar = "./kmeans.cl";
 	FILE * fp = fopen(tempchar, "rb"); 
 	if(!fp) { printf("ERROR: unable to open '%s'\n", tempchar); return -1; }
 	fread(source + strlen(source), sourcesize, 1, fp);
-	fclose(fp);
+	fclose(fp);*/
 		
 	// OpenCL initialization
 	int use_gpu = 1;
 	if(initialize(use_gpu)) return -1;
 
-	// compile kernel
+	/*// compile kernel
 	cl_int err = 0;
 	const char * slist[2] = { source, 0 };
 	cl_program prog = clCreateProgramWithSource(context, 1, slist, NULL, &err);
 	if(err != CL_SUCCESS) { printf("ERROR: clCreateProgramWithSource() => %d\n", err); return -1; }
 	err = clBuildProgram(prog, 0, NULL, NULL, NULL, NULL);
-	{ /* show warnings/errors
+	{  show warnings/errors
 		static char log[65536]; memset(log, 0, sizeof(log));
 		cl_device_id device_id = 0;
 //		err = clGetContextInfo(context, CL_CONTEXT_DEVICES, sizeof(device_id), &device_id, NULL);
 		clGetProgramBuildInfo(prog, device_id, CL_PROGRAM_BUILD_LOG, sizeof(log)-1, log, NULL);
 		if(err || strstr(log,"warning:") || strstr(log, "error:")) printf("<<<<\n%s\n>>>>\n", log);
-	*/}
+	}
 	if(err != CL_SUCCESS) { printf("ERROR: clBuildProgram() => %d\n", err); return -1; }
 	
 	char * kernel_kmeans_c  = "kmeans_kernel_c";
@@ -176,9 +263,9 @@ int allocate(int n_points, int n_features, int n_clusters, float **feature)
 	kernel_s = clCreateKernel(prog, kernel_kmeans_c, &err);  
 	if(err != CL_SUCCESS) { printf("ERROR: clCreateKernel() 0 => %d\n", err); return -1; }
 	kernel2 = clCreateKernel(prog, kernel_swap, &err);  
-	if(err != CL_SUCCESS) { printf("ERROR: clCreateKernel() 0 => %d\n", err); return -1; }
+	if(err != CL_SUCCESS) { printf("ERROR: clCreateKernel() 0 => %d\n", err); return -1; }*/
 		
-	clReleaseProgram(prog);	
+	clReleaseProgram(program);	
 	
 	d_feature = clCreateBuffer(context, CL_MEM_READ_WRITE, n_points * n_features * sizeof(float), NULL, &err );
 	if(err != CL_SUCCESS) { printf("ERROR: clCreateBuffer d_feature (size:%d) => %d\n", n_points * n_features, err); return -1;}
@@ -288,3 +375,67 @@ int	kmeansOCL(float **feature,    /* in: [npoints][nfeatures] */
 
 	return delta;
 }
+
+// Helper functions to display parameters returned by OpenCL queries
+static void device_info_ulong( cl_device_id device, cl_device_info param, const char* name) {
+   cl_ulong a;
+   clGetDeviceInfo(device, param, sizeof(cl_ulong), &a, NULL);
+   printf("%-40s = %lu\n", name, a);
+}
+static void device_info_uint( cl_device_id device, cl_device_info param, const char* name) {
+   cl_uint a;
+   clGetDeviceInfo(device, param, sizeof(cl_uint), &a, NULL);
+   printf("%-40s = %u\n", name, a);
+}
+static void device_info_bool( cl_device_id device, cl_device_info param, const char* name) {
+   cl_bool a;
+   clGetDeviceInfo(device, param, sizeof(cl_bool), &a, NULL);
+   printf("%-40s = %s\n", name, (a?"true":"false"));
+}
+static void device_info_string( cl_device_id device, cl_device_info param, const char* name) {
+   char a[STRING_BUFFER_LEN]; 
+   clGetDeviceInfo(device, param, STRING_BUFFER_LEN, &a, NULL);
+   printf("%-40s = %s\n", name, a);
+}
+
+// Query and display OpenCL information on device and runtime environment
+static void display_device_info( cl_device_id device ) {
+
+   printf("Querying device for info:\n");
+   printf("========================\n");
+   device_info_string(device, CL_DEVICE_NAME, "CL_DEVICE_NAME");
+   device_info_string(device, CL_DEVICE_VENDOR, "CL_DEVICE_VENDOR");
+   device_info_uint(device, CL_DEVICE_VENDOR_ID, "CL_DEVICE_VENDOR_ID");
+   device_info_string(device, CL_DEVICE_VERSION, "CL_DEVICE_VERSION");
+   device_info_string(device, CL_DRIVER_VERSION, "CL_DRIVER_VERSION");
+   device_info_uint(device, CL_DEVICE_ADDRESS_BITS, "CL_DEVICE_ADDRESS_BITS");
+   device_info_bool(device, CL_DEVICE_AVAILABLE, "CL_DEVICE_AVAILABLE");
+   device_info_bool(device, CL_DEVICE_ENDIAN_LITTLE, "CL_DEVICE_ENDIAN_LITTLE");
+   device_info_ulong(device, CL_DEVICE_GLOBAL_MEM_CACHE_SIZE, "CL_DEVICE_GLOBAL_MEM_CACHE_SIZE");
+   device_info_ulong(device, CL_DEVICE_GLOBAL_MEM_CACHELINE_SIZE, "CL_DEVICE_GLOBAL_MEM_CACHELINE_SIZE");
+   device_info_ulong(device, CL_DEVICE_GLOBAL_MEM_SIZE, "CL_DEVICE_GLOBAL_MEM_SIZE");
+   device_info_bool(device, CL_DEVICE_IMAGE_SUPPORT, "CL_DEVICE_IMAGE_SUPPORT");
+   device_info_ulong(device, CL_DEVICE_LOCAL_MEM_SIZE, "CL_DEVICE_LOCAL_MEM_SIZE");
+   device_info_ulong(device, CL_DEVICE_MAX_CLOCK_FREQUENCY, "CL_DEVICE_MAX_CLOCK_FREQUENCY");
+   device_info_ulong(device, CL_DEVICE_MAX_COMPUTE_UNITS, "CL_DEVICE_MAX_COMPUTE_UNITS");
+   device_info_ulong(device, CL_DEVICE_MAX_CONSTANT_ARGS, "CL_DEVICE_MAX_CONSTANT_ARGS");
+   device_info_ulong(device, CL_DEVICE_MAX_CONSTANT_BUFFER_SIZE, "CL_DEVICE_MAX_CONSTANT_BUFFER_SIZE");
+   device_info_uint(device, CL_DEVICE_MAX_WORK_ITEM_DIMENSIONS, "CL_DEVICE_MAX_WORK_ITEM_DIMENSIONS");
+   device_info_uint(device, CL_DEVICE_MEM_BASE_ADDR_ALIGN, "CL_DEVICE_MEM_BASE_ADDR_ALIGN");
+   device_info_uint(device, CL_DEVICE_MIN_DATA_TYPE_ALIGN_SIZE, "CL_DEVICE_MIN_DATA_TYPE_ALIGN_SIZE");
+   device_info_uint(device, CL_DEVICE_PREFERRED_VECTOR_WIDTH_CHAR, "CL_DEVICE_PREFERRED_VECTOR_WIDTH_CHAR");
+   device_info_uint(device, CL_DEVICE_PREFERRED_VECTOR_WIDTH_SHORT, "CL_DEVICE_PREFERRED_VECTOR_WIDTH_SHORT");
+   device_info_uint(device, CL_DEVICE_PREFERRED_VECTOR_WIDTH_INT, "CL_DEVICE_PREFERRED_VECTOR_WIDTH_INT");
+   device_info_uint(device, CL_DEVICE_PREFERRED_VECTOR_WIDTH_LONG, "CL_DEVICE_PREFERRED_VECTOR_WIDTH_LONG");
+   device_info_uint(device, CL_DEVICE_PREFERRED_VECTOR_WIDTH_FLOAT, "CL_DEVICE_PREFERRED_VECTOR_WIDTH_FLOAT");
+   device_info_uint(device, CL_DEVICE_PREFERRED_VECTOR_WIDTH_DOUBLE, "CL_DEVICE_PREFERRED_VECTOR_WIDTH_DOUBLE");
+
+   {
+      cl_command_queue_properties ccp;
+      clGetDeviceInfo(device, CL_DEVICE_QUEUE_PROPERTIES, sizeof(cl_command_queue_properties), &ccp, NULL);
+      printf("%-40s = %s\n", "Command queue out of order? ", ((ccp & CL_QUEUE_OUT_OF_ORDER_EXEC_MODE_ENABLE)?"true":"false"));
+      printf("%-40s = %s\n", "Command queue profiling enabled? ", ((ccp & CL_QUEUE_PROFILING_ENABLE)?"true":"false"));
+   }
+}
+
+
