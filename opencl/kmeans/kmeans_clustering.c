a8c8a855a6d8a58fdd55263572d48f110f841022
Jorge <646122@unizar.es>
2017-04-15 12:28:33 +0200

    Global memory

--- opencl/kmeans/kmeans_clustering.c
+++ opencl/kmeans/kmeans_clustering.c
@@ -1,4 +1,3 @@
-
 /*****************************************************************************/
 /*IMPORTANT:  READ BEFORE DOWNLOADING, COPYING, INSTALLING OR USING.         */
 /*By downloading, copying, installing or using the software you agree        */
